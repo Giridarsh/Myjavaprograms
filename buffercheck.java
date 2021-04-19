7520e25739884be4cad6320e835084e6b9007f70
Giridarsh <79186113+Giridarsh@users.noreply.github.com>
2021-04-19 12:06:15 +0530

    Add files via upload

---
+++ buffercheck.java
@@ -0,0 +1,18 @@
+class check
+{ 
+      StringBuffer str = new StringBuffer("GIRIDARSH");
+      check()
+      {
+            System.out.println(str.indexOf(6));
+      }
+}
+  
+
+class buffercheck
+{
+      static public void main(String asd[])
+      {
+            check obj = new check();
+      }
+}
+            
