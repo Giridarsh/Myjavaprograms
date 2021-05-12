36e74483deff39d90184cf52413c4487a7249701
Giridarsh <79186113+Giridarsh@users.noreply.github.com>
2021-05-12 11:10:31 +0530

    Add files via upload

---
+++ democollection.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class democollection
+{
+      public static void main(String[] args)
+      {
+            ArrayList a = new ArrayList();
+            a.add("ab");
+            a.add("cd");
+            a.add("ef");
+            System.out.println(a);
+      }
+}
