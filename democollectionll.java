aaca4d9d2cf700c97e43e60261bc03681ec5a6d3
Giridarsh <79186113+Giridarsh@users.noreply.github.com>
2021-05-15 22:38:06 +0530

    Add files via upload

---
+++ democollectionll.java
@@ -0,0 +1,14 @@
+import java.util.* ;
+class democollectionll
+{
+  public static void main(String[] args)
+  {
+    LinkedList< String> ll = new LinkedList< String>();
+    ll.add("a");
+    ll.add("b");
+    ll.add("c");
+    ll.addLast("z");
+    ll.addFirst("A");
+    System.out.println(ll);
+  }
+}
