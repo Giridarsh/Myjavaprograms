c1769584b2f7e022ace64f500730cb3f5bd5db87
Giridarsh <giridarsh98@gmail.com>
2021-07-16 00:44:42 -0700

    Add files via upload

---
+++ oneplus.java
@@ -0,0 +1,24 @@
+class phone
+{
+    void mobile()
+    {
+        int a=49,b=23;
+        if(a<b)
+        {
+            System.out.println("The solution is true");
+        }
+        else
+        {
+            System.out.println("False");
+        }
+    }
+}
+class oneplus
+{
+    public static void main(String args[])
+    {
+        phone obj = new phone();
+        obj.mobile();
+    }
+}
+        
