445e0813ea1b569f6d62fc666eecd8c29ce1c081
Giridarsh <giridarsh98@gmail.com>
2021-07-20 13:34:03 +0530

    Add files via upload

--- demoawt.java
+++ demoawt.java
@@ -2,20 +2,24 @@ import java.awt.*;
 
 class set extends Frame
 {
-    Label l1 = new Label("First Name : ");
-    Label l2 = new Label("Last Name : "); 
-    Label l3 = new Label("Email ID : ");
-    Label l4 = new Label("Password : ");
-    Label l5 = new Label("Date of Birth : ");
-    Label l6 = new Label("Blood Group : ");
-    Button b1 =  new Button("Submit");
+    JLabel l1 = new JLabel("First Name : ");
+    JLabel l2 = new JLabel("Last Name : "); 
+    JLabel l3 = new JLabel("Email ID : ");
+    JLabel l4 = new JLabel("Password : ");
+    JLabel l5 = new JLabel("Date of Birth : ");
+    JLabel l6 = new JLabel("Blood Group : ");
+    JLabel l7 = new JLabel("Gender : ");
+    JCheckbox c1 = new JCheckbox("Male", true);
+    JCheckbox c2 = new JCheckbox("Female");
+    JButton b1 =  new JButton("Submit");
 
-    TextField t1 = new TextField();
-    TextField t2 = new TextField();
-    TextField t3 = new TextField();
-    TextField t4 = new TextField();
-    TextField t5 = new TextField();
-    TextField t6 = new TextField();
+    JTextField t1 = new JTextField();
+    JTextField t2 = new JTextField();
+    JTextField t3 = new JTextField();
+    JTextField t4 = new JTextField();
+    JTextField t5 = new JTextField();
+    JTextField t6 = new JTextField();
+    JTextField t7 = new JTextField();
     
     set()
     {
@@ -25,7 +29,7 @@ class set extends Frame
         setBackground(Color.yellow);
         setTitle("Form");
 
-        Panel p1 = new Panel(new GridLayout(6,2));
+        JPanel p1 = new JPanel(new GridLayout(6,2));
         p1.add(l1);
         p1.add(t1);
         p1.add(l2);
@@ -38,13 +42,19 @@ class set extends Frame
         p1.add(t5);
         p1.add(l6);
         p1.add(t6);
+        p1.add(l7);
+        p1.add(t7);
         t4.setEchoChar('*');
         p1.setBounds(100,100,300,120);
-        add(p1);
-        add(b1);
+        c1.setBounds(200,200,300,120);
+        c1.setSize(70,30);
+        add(c1);
+        c2.setBounds(100,100,300,120);
+        c2.setSize(100,100);
+        add(c2);
         b1.setBounds(250,250,70,30);
-     
-
+        add(b1);
+        add(p1);
     }
 }
 
