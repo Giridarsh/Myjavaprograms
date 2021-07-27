c048307434d7553039f14430489f76b23ef6c787
Giridarsh <giridarsh98@gmail.com>
2021-07-27 11:32:04 +0530

    Add files via upload

--- mousekeylistener.java
+++ mousekeylistener.java
@@ -5,7 +5,6 @@ import java.util.Formatter;
 import javax.swing.*;
 import java.io.File;
 import javax.sound.sampled.*;
-import helpers.GlobalProperties;
 import java.awt.Color;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -29,158 +28,201 @@ public class mousekeylistener extends JPanel implements KeyListener,MouseListene
     private float ballY = 290 - ballRadius;
     private float ballSpeedX = 6;
     private float ballSpeedY = 6;
+    //Random number
     private int randomNumber=0;
+
+
     private boolean randomColorMode = false;
     private Color ballColor = null, numberColor = null;
     private Color backgroundColor;
     private static final float N = 64;
     private final Queue<Color> clut = new LinkedList<Color>();
+    //Timer timer2 = new Timer(3*1000);
     Timer timer2;
+
+
     JMenuBar menubar = new JMenuBar();
     JMenu menu = new JMenu("menu");
-    public MovingLeftRight()
-    {
-        JMenu speed = new JMenu("speed");
-        JMenu size = new JMenu("Size");
-        menu.add(speed);
-        menu.add(size);
-        JMenuItem big = new JMenuItem("Big");
-        JMenuItem small = new JMenuItem("small");
-        JMenuItem middium = new JMenuItem("Medium");
-        JMenuItem fast = new JMenuItem("Fast");
-        JMenuItem normal = new JMenuItem("Normal");
-        JMenuItem slow = new JMenuItem("Slow");
-        speed.add(fast);
-        speed.add(normal);
-        speed.add(slow);
-        size.add(big);
-        size.add(small);
-        size.add(middium);
-        menubar.add(menu);
-        this.addKeyListener(this);
+    public mousekeylistener() {
+
+    JMenu speed = new JMenu("speed");
+    JMenu size = new JMenu("Size");
+    menu.add(speed);
+    menu.add(size);
+    JMenuItem big = new JMenuItem("Big");
+    JMenuItem small = new JMenuItem("small");
+    JMenuItem middium = new JMenuItem("Medium");
+
+    JMenuItem fast = new JMenuItem("Fast");
+    JMenuItem normal = new JMenuItem("Normal");
+    JMenuItem slow = new JMenuItem("Slow");
+
+    speed.add(fast);
+    speed.add(normal);
+    speed.add(slow);
+
+    size.add(big);
+    size.add(small);
+    size.add(middium);
+    menubar.add(menu);
+    this.addKeyListener(this);
+
+
         this.setPreferredSize(new Dimension(BOX_WIDTH, BOX_HEIGHT));
+        // Prepare a Clip
+
         try {
-            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(soundFile);
-            AudioInputStream audioInputStream2 = AudioSystem.getAudioInputStream(soundFile2);
+            AudioInputStream audioInputStream =
+                AudioSystem.getAudioInputStream(soundFile);
+            AudioInputStream audioInputStream2 =
+                AudioSystem.getAudioInputStream(soundFile2);
             AudioFormat audioFormat = audioInputStream.getFormat();
-            AudioFormat audioFormat2 = audioInputStream2.getFormat(); 
-            DataLine.Info dataLineInfo = new DataLine.Info(Clip.class, audioFormat);
-            DataLine.Info dataLineInfo2 = new DataLine.Info(Clip.class, audioFormat2);
+            AudioFormat audioFormat2 = audioInputStream2.getFormat();
+            DataLine.Info dataLineInfo =
+                new DataLine.Info(Clip.class, audioFormat);
+            DataLine.Info dataLineInfo2 =
+                new DataLine.Info(Clip.class, audioFormat2);
             clip = (Clip) AudioSystem.getLine(dataLineInfo);
             clip2 = (Clip) AudioSystem.getLine(dataLineInfo2);
             clip.open(audioInputStream);
             clip2.open(audioInputStream2);
-        } 
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace(System.err);
         }
-        Timer timer = new Timer(1000 / RATE, new ActionListener()
-        {
-            public void actionPerformed(ActionEvent e)
-            {
+        Timer timer = new Timer(1000 / RATE, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
                 ballX += ballSpeedX;
                 ballY += ballSpeedY;
-                if (ballX - ballRadius < 0)
-                {
+                if (ballX - ballRadius < 0) {
                     ballSpeedX = -ballSpeedX;
                     ballX = ballRadius;
                     playSound();
-                }
-                else if (ballX + ballRadius > BOX_WIDTH)
-                {
+                    //getParameters();
+                } else if (ballX + ballRadius > BOX_WIDTH) {
                     ballSpeedX = -ballSpeedX;
                     ballX = BOX_WIDTH - ballRadius;
+                    //playSound2();
+                    //getParameters();
                 }
-                if (ballY - ballRadius < 0)
-                {
+                if (ballY - ballRadius < 0) {
                   ballSpeedY = -ballSpeedY;
                   ballY = ballRadius ;
-                }
-                else if (ballY + ballRadius > BOX_HEIGHT)
-                {
+                  //playSound2();
+               } else if (ballY + ballRadius > BOX_HEIGHT) {
                   ballSpeedY = -ballSpeedY;
                   ballY = BOX_HEIGHT - ballRadius;
                }
+
                 repaint();
+
             }
         });
-        Timer timer2 = new Timer(4000, new ActionListener()
-        {
-            public void actionPerformed(ActionEvent e)
-            {
+        //timer.start();
+
+
+        Timer timer2 = new Timer(4000, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
                randomNumber=(int) Math.round(Math.random()*9);
+
+
             }
         });
         timer2.start();
-        fast.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent event)
-            {
+
+
+
+        fast.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent event) {
+
+
             ballSpeedX = 9;
             ballSpeedY = 9;
+            //repaint();
+
+
+
         }
       });
-       slow.addActionListener(new ActionListener()
-       {
-           public void actionPerformed(ActionEvent event)
-           {
+       slow.addActionListener(new ActionListener() {
+          public void actionPerformed(ActionEvent event) {
+
              ballSpeedX = 3;
              ballSpeedY = 3;
+
           }
         });
-       normal.addActionListener(new ActionListener()
-       {
-           public void actionPerformed(ActionEvent event)
-           {
+       normal.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent event) {
+
            ballSpeedX =  5;
            ballSpeedY = 5;
+
+
+
         }
       });
-       big.addActionListener(new ActionListener()
-       {
-           public void actionPerformed(ActionEvent event)
-           {
-               ballRadius = 50;
+       big.addActionListener(new ActionListener() {
+          public void actionPerformed(ActionEvent event) {
+
+            /*if(Selection.equals("Big")){
+             */
+              ballRadius = 50;
+
+
+
           }
         });
-       small.addActionListener(new ActionListener()
-       {
-           public void actionPerformed(ActionEvent event)
-           {
-               ballRadius = 10;
-           }
-        });
-        middium.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent event)
-            {
-                ballRadius = 30;
+       small.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent event) {
+
+          /*if(Selection.equals("Big")){
+           */
+            ballRadius = 10;
+
+
+
+        }
+      });
+
+       middium.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent event) {
+
+          /*if(Selection.equals("Big")){
+           */
+            ballRadius = 30;
+
+
+
         }
       });
       timer.start();
+
+
+
     }
-    private void playSound() 
-    {
-        Runnable soundPlayer = new Runnable()
-        {
-            public void run()
-            {
+
+
+
+    // Play the sound in a separate thread.
+    private void playSound() {
+        Runnable soundPlayer = new Runnable() {
+            @Override
+            public void run() {
                 try {
                     clip.setMicrosecondPosition(0);
                     clip.start();
-                } 
-                catch (Exception e)
-                {
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
         };
         new Thread(soundPlayer).start();
     }
-    private void playSound2()
-    {
+    private void playSound2() {
         Runnable soundPlayer2 = new Runnable() {
+            @Override
             public void run() {
                 try {
                     clip2.setMicrosecondPosition(0);
@@ -192,67 +234,91 @@ public class mousekeylistener extends JPanel implements KeyListener,MouseListene
         };
         new Thread(soundPlayer2).start();
     }
-    public void paintComponent(Graphics g)
-    {
-   }
-   super.paintComponent(g);
-   g.setColor(Color.BLACK);
-   g.fillRect(0, 0, BOX_WIDTH, BOX_HEIGHT);
-   g.setColor(Color.BLUE);
-   g.fillOval((int) (ballX - ballRadius), (int) (ballY - ballRadius), (int) (2 * ballRadius), (int) (2 * ballRadius));
-   g.setColor(Color.WHITE);
-   int fontSize = (int) ballRadius;
-   Font font = new Font("Arial", Font.PLAIN, fontSize);
-   g.setFont(font);
-   g.drawString(("" + randomNumber), (int) (ballX-ballRadius/3), (int) (ballY+ballRadius/3));
-   g.setFont(new Font("Dialog", Font.PLAIN, 12));
-   StringBuilder sb = new StringBuilder();
-   Formatter formatter = new Formatter(sb);
-   formatter.format("Ball @(%3.0f, %3.0f) Speed=(%2.0f, %2.0f)", ballX, ballY, ballSpeedX, ballSpeedY);
-   g.drawString(sb.toString(), 20, 30);
- }
- public void keyPressed(KeyEvent e)
- {
-     int keyCode = e.getKeyCode();
-     if(keyCode == KeyEvent.VK_1)
-     {
-         System.out.printf("Correct!");
-      }
-      else
-      {
-          e.consume();
-      }
-  }
-  public void keyReleased(KeyEvent e)
-  {
-  }
-  public void keyTyped(KeyEvent e)
-  {
-  }
-  public void mouseClicked(MouseEvent arg0)
-  {
-  }
-  public void mouseEntered(MouseEvent arg0)
-  {
-  }
-  public void mouseExited(MouseEvent arg0)
-  {
-  }
-  public void mousePressed(MouseEvent e)
-  {
-  }
-  public void mouseReleased(MouseEvent arg0)
-  {
-  }
-  public static void main(String asd[])
-  {
-      javax.swing.SwingUtilities.invokeLater(new Runnable()
-      {
-          public void run()
-          {
-                MovingLeftRight Ani = new MovingLeftRight();
+
+
+
+    @Override
+    public void paintComponent(Graphics g) {
+       // for (int i = 0; i < N; i++) {
+    //clut.add(Color.getHSBColor(i / N, 1, 1));
+
+             //   }
+
+        super.paintComponent(g); // Paint background
+        //getColors();
+
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, BOX_WIDTH, BOX_HEIGHT);
+        g.setColor(Color.BLUE);
+        //clut.add(clut.remove());
+        g.fillOval(
+            (int) (ballX - ballRadius),
+            (int) (ballY - ballRadius),
+            (int) (2 * ballRadius), (int) (2 * ballRadius));
+        g.setColor(Color.WHITE);
+
+
+        int fontSize = (int) ballRadius;
+        Font font = new Font("Arial", Font.PLAIN, fontSize);
+        g.setFont(font);
+
+
+        g.drawString(
+                    ("" + randomNumber)
+                , (int) (ballX-ballRadius/3)
+                ,(int) (ballY+ballRadius/3));
+
+        g.setFont(new Font("Dialog", Font.PLAIN, 12));
+        StringBuilder sb = new StringBuilder();
+        Formatter formatter = new Formatter(sb);
+        formatter.format(
+            "Ball @(%3.0f, %3.0f) Speed=(%2.0f, %2.0f)", ballX, ballY, ballSpeedX,
+            ballSpeedY);
+        g.drawString(sb.toString(), 20, 30);
+    }
+    public void keyPressed(KeyEvent e) {
+		int keyCode = e.getKeyCode();
+		if(keyCode == KeyEvent.VK_1){
+                    System.out.printf("Correct!");
+		}else{
+			//mess="Pressed : "+KeyEvent.getKeyText(keyCode);
+			e.consume();
+
+		}
+
+
+	}
+	public void keyReleased(KeyEvent e) {
+
+
+	}
+	public void keyTyped(KeyEvent e) {
+
+	}
+        public void mouseClicked(MouseEvent arg0) {
+	}
+	public void mouseEntered(MouseEvent arg0) {}
+	public void mouseExited(MouseEvent arg0) {
+	}
+	public void mousePressed(MouseEvent e) {
+		//mess="you pressed down the mouse";
+
+	}
+	public void mouseReleased(MouseEvent arg0) {
+		//mess="you released the mouse";
+
+	}
+    public static void main(String[] args) {
+
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            @Override
+
+            public void run() {
+                mousekeylistener Ani = new mousekeylistener();
                 JFrame frame = new JFrame("A Moving Ball");
                 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                //frame.setContentPane(new AnimationSound());
+
                 frame.add(Ani);
                 frame.setJMenuBar(Ani.menubar);
                 frame.pack();
@@ -260,4 +326,5 @@ public class mousekeylistener extends JPanel implements KeyListener,MouseListene
             }
         });
     }
+
 }
